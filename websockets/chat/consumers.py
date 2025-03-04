8cea8148d48a3236c31cd8e5dcc284cf6fbcb478
hirwa cedric <hirwacedr12@gmail.com>
2025-03-04 21:15:58 +0200

    Refactor ChatConsumer to enhance message handling and add message creation logic; update room title in template

--- websockets/chat/consumers.py
+++ websockets/chat/consumers.py
@@ -20,23 +20,35 @@ class ChatConsumer(AsyncWebsocketConsumer):
             )
             await self.close(code)
         
-        async def receive(self, data_received):
-            data_json = json.loads(data_received)
-            message = data_json['message']
-            username = data_json['username']
-            room = await self.get_room()
-            await self.save_message(room, username, message)
+        async def receive(self, text_data):
+            print("Received data")
+            data_json = json.loads(text_data)
+            print(data_json)
+            event={
+                'type': 'send_message',
+                'message': data_json['message'],
+                'sender': data_json['sender']
+            }
             await self.channel_layer.group_send(
                 self.room_group_name,
-                {
-                    'type': 'chat_message',
-                    'message': message,
-                    'username': username
-                }
+                event
             )
+        async def send_message(self, event):
+            data = event["message"]
+
+            await self.create_message(data = data)
+            response = {
+                'message': data,
+                'sender': data["sender"]
+            }
+
+            await self.send(text_data=json.dumps(response))
             
-           
-
-
-
-
+        @database_sync_to_async
+        def create_message(self, data):
+            get_room = Room.objects.get(room_name=data['room_name'])
+            if not Message.objects.filter(room=get_room, sender=data['sender'], message=data['message']).exists():
+                new_message = Message.objects.create(room=get_room, sender=data['sender'], message=data['message'])
+                return new_message
+            return None
+            
