a2788418f80e8d747cc30446d8f08fe309c39b80
hirwa cedric <hirwacedr12@gmail.com>
2025-03-04 14:35:42 +0200

    Add message retrieval to chat room view for improved user experience

--- websockets/chat/views.py
+++ websockets/chat/views.py
@@ -16,7 +16,11 @@ def index(request):
     return render(request, 'chat/index.html')
 
 def room(request, room_name, username):
-    return render(request, 'chat/room.html', {
+    existing_room = Room.objects.get(room_name__icontains=room_name)
+    messages = Message.objects.filter(room=existing_room)
+    context = {
         'room_name': room_name,
-        'username': username
-    })
+        'user': username,
+        'messages': messages
+    }
+    return render(request, 'chat/room.html', context)
