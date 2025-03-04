e73a24e8865c565797cececdd61eb5190aacaad6
hirwa cedric <hirwacedr12@gmail.com>
2025-03-04 18:24:04 +0200

    Add WebSocket support for chat functionality with ChatConsumer class

---
+++ websockets/chat/consumers.py
@@ -0,0 +1,26 @@
+import json
+from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.db import database_sync_to_async
+from .models import Message, Room
+
+class ChatConsumer(AsyncWebsocketConsumer):
+        async def connect(self):
+            self.room_name = f"room_{self.scope['url_route']['kwargs']['room_name']}"
+            self.room_group_name = f'chat_{self.room_name}'
+            await self.channel_layer.group_add(
+                self.room_group_name,
+                self.channel_name
+            )
+            await self.accept()
+        
+        async def disconnect(self, code):
+            await self.channel_layer.group_discard(
+                self.room_group_name,
+                self.channel_name
+            )
+            await self.close(code)
+           
+
+
+
+
