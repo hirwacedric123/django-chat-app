64dd813238120a841a16827e2b851dd72a975158
hirwa cedric <hirwacedr12@gmail.com>
2025-03-04 13:35:47 +0200

    Register Room and Message models in the Django admin

--- websockets/chat/admin.py
+++ websockets/chat/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Room, Message
 
 # Register your models here.
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ('room_name',)
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('room', 'sender', 'created_at')
